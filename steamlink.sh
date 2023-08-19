3117da402965dec0ed400adafa44dc91ffde9cfd
Johan Odendaal <106447856+pops199@users.noreply.github.com>
2023-08-19 22:12:46 +0200

    Update steamlink.sh

--- steamlink.sh
+++ steamlink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-  xdotool key Control+Alt+F5
+  xdotool key Control+Alt+F2
   setsid sh -c '/home/user/bin/steamlink <> /dev/tty2 >&0'
   # /home/user/bin/steamlink is waarokal die steamlink executable le
 exit 0
